490095f570c82ceb4642bcfa943e4db3e2578ba1
Jochen Thaeder <jochen@thaeder.de>
2016-09-02 08:55:20 -0700

    add usage

--- diskToXRootD/tools/rmFilesFromXRD.sh
+++ diskToXRootD/tools/rmFilesFromXRD.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -l 
 #
-# rm files from XRootD
-# copied correctly
+# rm files from XRootD using an in List as argument
 #
+# - Argument: inList of files on XRD Disk
+#     -> Dump of "fileFullPath" column from MongoDB
+#
+#   Valid file layouts:
+#
+#      fileFullPath
+#      "/export/data/xrd/ns/star/reco/ppProductionMB62/....1060005.MuDst.root"
+#      "/export/data/xrd/ns/star/reco/ppProductionMB62/....1020001.MuDst.root"
+#      ....
+#
+#      /export/data/xrd/ns/star/reco/ppProductionMB62/....1060005.MuDst.root
+#      /export/data/xrd/ns/star/reco/ppProductionMB62/....1020001.MuDst.root
+#      ....
+#     
 # Author: Jochen Thaeder                                           
 
 baseDir=/global/homes/j/jthaeder/SDMS/diskToXRootD
@@ -24,8 +37,23 @@ input=$1
 
 # -- Loop over inList
 while read -r line ; do 
-    tmp=`echo $line | awk -F"picodsts" '{ print $2 }'`
-    path="/star/picodsts${tmp}"
+
+    echo $line | grep "^\"" > /dev/null
+    if [ $? -eq 0 ] ; then 
+	path=`echo $line | awk -F'"' '{ print $2 }'`
+    else
+	path=$line
+    fi
+    
+    if [ "$path" == "fileFullPath" ] ; then 
+	continue
+    fi
+
+    echo $path | grep "^/export/data/xrd/ns/" > /dev/null
+    if [ $? -eq 0 ] ; then 
+	pathX=`echo $path | awk -F'/export/data/xrd/ns/' '{ print $2 }'`
+	path=$pathX
+    fi
 
     echo $path
 
